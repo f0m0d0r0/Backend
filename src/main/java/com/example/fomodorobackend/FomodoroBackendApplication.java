8fdeb6d8025a771db3e66f05302fccf53b40d44b
Ben Hwang <benhwang@usc.edu>
2022-02-05 10:42:15 -0800

    spring initialization

---
+++ src/main/java/com/example/fomodorobackend/FomodoroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fomodorobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomodoroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FomodoroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fomodorobackend/FomodoroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fomodorobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FomodoroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
