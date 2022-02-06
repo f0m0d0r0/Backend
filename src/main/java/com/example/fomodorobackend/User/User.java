9588bbd2b950314450a9b4f82586b1b8241418c6
Ben Hwang <benhwang@usc.edu>
2022-02-05 16:24:27 -0800

    added cloud firebase support

--- src/main/java/com/example/fomodorobackend/User/User.java
+++ src/main/java/com/example/fomodorobackend/User/User.java
@@ -1,16 +1,9 @@
 package com.example.fomodorobackend.User;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
 public class User {
 
-    @Id
-    private long id;
-
-    @Indexed(unique = true)
     private String username;
 
     private String password;
@@ -18,19 +11,10 @@ public class User {
     private int points;
 
 
-    public User(long id, String username, String password, int points) {
-        this.id = id;
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.points = points;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
+        this.points = 0;
     }
 
     public String getUsername() {
